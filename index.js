7d2526b4ca55a3ff4388cf4dee80d9d9887d69ed
Lane Pemberton <lanebpemberton@gmail.com>
2021-04-20 19:54:52 -0400

    Added some js structure to script

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 //require inquirer package to gather command line information
 const inquirer = require('inquirer');
+//require file system to write read me file
+const fs = require('fs');
+
+//append data to readme file
+function appendDataToReadMe(data)
+{
+    fs.appendFileSync("ReadMe.md",data);
+}
+
+//append title to readme
+function appendTitleToReadMe(title)
+{
+    appendDataToReadMe(title);
+}
+
+//append link to read me
+function appendLinkToReadMe(link)
+{
+    
+}
 
 //WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
 
 inquirer
   .prompt([
+    //declaring my list of questions here
       {
         name:"title",
         type:"input",
@@ -41,14 +62,14 @@ inquirer
           message: "Select what type of project you're describing",
           choices:["Node Project", "C# Project", "React Project"]
       }*/
-    /* Pass your questions in here */
   ])
   .then(answers => {
-    // Use user feedback for... whatever!!
+    //deal with each user input in turn
     console.log(answers.projectTitle);
     console.log(answers.projectType);
   })
   .catch(error => {
+      //boiler plate error handling from inquire
     if(error.isTtyError) {
       // Prompt couldn't be rendered in the current environment
     } else {
